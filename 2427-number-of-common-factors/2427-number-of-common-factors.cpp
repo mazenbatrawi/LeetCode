817cf11f71ea7e33ea2fc701b0a8062d90a99d9d
mazenbatrawi <71159005+mazenbatrawi@users.noreply.github.com>
2022-10-03 20:59:27 +0300

    Time: 6 ms (10.00%), Space: 5.9 MB (60.00%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.cpp
+++ 2427-number-of-common-factors/2427-number-of-common-factors.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int commonFactors(int a, int b) {
         int ans = 0;
-        for(int i = 1; i <= max(a, b); i++){
+        for(int i = 1; i <= min(a, b); i++){
             ans += (a % i == 0 && b % i == 0);
         }
         return ans;
