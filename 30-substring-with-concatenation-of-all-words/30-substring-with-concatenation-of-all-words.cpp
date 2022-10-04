1ed1f5bf9ff6e6f4aa87d11e2cd8f6102ef62d57
mazenbatrawi <71159005+mazenbatrawi@users.noreply.github.com>
2022-10-05 02:02:01 +0300

    Time: 2187 ms (5.01%), Space: 340.8 MB (12.92%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -4,13 +4,13 @@ public:
         vector<int> ans;
         int n = words.size(), m = words[0].size();
         int add = n * m;
-        map<string, int> has;
+        unordered_map<string, int> has;
         for(string t : words){
             has[t] += 1;
         }
         for(int i = 0; i + add - 1 < (int) s.size(); i++){
             string t = s.substr(i, add);
-            map<string, int> has2 = has;
+            unordered_map<string, int> has2 = has;
             for(int j = i; j <= i + add - 1; j += m){
                 string temp = s.substr(j, m);
                 if(!has.count(temp)){
