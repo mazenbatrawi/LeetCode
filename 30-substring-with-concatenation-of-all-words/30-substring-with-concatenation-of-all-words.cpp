fc693d403746bdee77ba10e031d50d6eeb657f3c
mazenbatrawi <71159005+mazenbatrawi@users.noreply.github.com>
2022-10-05 02:01:24 +0300

    Time: 2623 ms (5.01%), Space: 344.7 MB (12.04%) - LeetHub

---
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> ans;
+        int n = words.size(), m = words[0].size();
+        int add = n * m;
+        map<string, int> has;
+        for(string t : words){
+            has[t] += 1;
+        }
+        for(int i = 0; i + add - 1 < (int) s.size(); i++){
+            string t = s.substr(i, add);
+            map<string, int> has2 = has;
+            for(int j = i; j <= i + add - 1; j += m){
+                string temp = s.substr(j, m);
+                if(!has.count(temp)){
+                    break;
+                }
+                if(has2[temp] == 1){
+                    has2.erase(temp);
+                }
+                else{
+                    has2[temp] -= 1;
+                }
+            }
+            if(has2.empty()){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
