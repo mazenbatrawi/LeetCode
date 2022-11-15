48ff3e9b84892b4d044099617ec7a29e6066b7fd
mazenbatrawi <71159005+mazenbatrawi@users.noreply.github.com>
2022-11-15 14:23:27 +0200

    Time: 320 ms (30.02%), Space: 11.2 MB (32.95%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -4,15 +4,11 @@ public:
         if(l > r){
             return 0;
         }
-        if(l == r){
-            return k == 1;
-        }
         int n = s.size();
         vector<int> cnt(26, 0);
         for(int i = l; i <= r; i++){
             cnt[s[i] - 'a'] += 1;
         }
-        int mx = 0;
         for(int i = l; i <= r; i++){
             if(cnt[s[i] - 'a'] < k){
                 return max(solve(l, i - 1, s, k), solve(i + 1, r, s, k));
