1302b8dcf87f319e5f4b0e7b180c7de7487a48c3
mazenbatrawi <71159005+mazenbatrawi@users.noreply.github.com>
2022-11-15 20:15:36 +0200

    Time: 247 ms (9.90%), Space: 61.3 MB (70.05%) - LeetHub

---
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool ovf(int a, int b, int k){
+        return a * b >= k;
+    }
+    
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if(k == 0){
+            return 0;
+        }
+        int n = nums.size();
+        int l = 0, r = 0, p = 1, ans = 0;
+        while(r < n){
+            p *= nums[r];
+            while(l <= r && p >= k){
+                p /= nums[l++];
+            }
+            ans += r++ - l + 1;
+        }
+        return ans;
+    }
+};
